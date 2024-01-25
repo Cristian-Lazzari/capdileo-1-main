e7b661004f2be1b3c799c4149aecaec08a4fa0a5
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2024-01-25 10:45:01 +0100

    template1

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script >
+  
+  import AppHeader from './components/AppHeader.vue'
+
+
+  export default {
+  components:{  AppHeader }
+  }
+</script>
+
+<template>
+
+  <div class="container">
+    <AppHeader />
+    <router-view class="page"></router-view>
+  </div>
+ 
+
+  
+</template>
+
+<style lang="scss">
+@use './assets/styles/general.scss' as *;
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+
+<script >
+  
+</script>
+
+
+<template>
+  
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/styles/general.scss' as *;
+
+
+</style>
